ee1f339827a36bf304d75a4380006783586ed7f0
Nacer MEDIOUNI <nacer.mediouni@gmail.com>
2022-05-24 14:30:07 +0100

    adding card and hero components

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <div className="card">
+      <img src="../assets/katie-zaferes.png" className="card--image" alt="" />
+      <div className="card--stats">
+        <img src="../assets/star.png" className="card--star" alt="" />
+        <span>5.0</span>
+        <span className="gray">(6) • </span>
+        <span className="gray">USA</span>
+      </div>
+      <p>Life Lessons with Katie Zaferes</p>
+      <p>
+        <span className="bold">From $136</span> / person
+      </p>
+    </div>
+  );
+}
